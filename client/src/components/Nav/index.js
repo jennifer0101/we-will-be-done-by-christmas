527fe15a126f4b822babcd0a61d948d351fde138
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-12-05 20:28:17 -0600

    react front end, images

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function Nav() {
+    return (
+        <nav className="navbar">
+            <Link className="navbar-brand" to="/">
+                Neighborhood Pantry
+        </Link>
+            <div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link
+                            to="/"
+                            className={
+                                window.location.pathname === "/" || window.location.pathname === "home"
+                                    ? "nav-link active"
+                                    : "nav-link"
+                            }
+                        >
+                            Neighborhood Pantry
+                    </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                        to="/search"
+                        className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
+                        >
+                            Search Items
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
---
+++ client/src/pages/search.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+function home() {
+  return (
+    <div>
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-12">
+            <h1>Neighborhood Pantry</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default home;
