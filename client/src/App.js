8d378dfe7de417e567952e557337c6c2cbc2a7a5
Matt Joseph <mattomjoe@yahoo.com>
2019-12-08 22:31:33 -0600

    Move home component rendering to login page.

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
 import NavBar from "./components/NavBar";
 import { useAuth0 } from "./react-auth0-spa";
+import Carousel from "./components/Carousel";
 
 
 function App() {
@@ -22,14 +23,22 @@ function App() {
       <div>
         <NavBar />
 
-        { isAuthenticated &&
+        { isAuthenticated ?
           <div>
             <Nav />
             <Wrapper>
-              <Route exact path="/" component={home} />
+              {/* <Route exact path="/" component={home} /> */}
               <Route exact path="/search" component={search} />
               <Route exact path="/addItems" component={addItems} />
               <Route exact path="/inviteFriends" component={inviteFriends} />
+            </Wrapper>
+          </div>
+          :
+          <div>
+            <Wrapper>
+              {/* <h1>Login Test</h1> */}
+              <Route exact path="/" component={home} />
+              {/* <Route exact path="/" component={Carousel} /> */}
             </Wrapper>
           </div>
         }
@@ -42,28 +51,3 @@ function App() {
 
 
 export default App;
-
-
-
-
-// import React, { Component } from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
