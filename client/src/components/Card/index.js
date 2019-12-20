b0e3c5bf752607ab5fe0280289cc8164479054c4
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-12-19 21:48:56 -0600

    styling issues

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./style.css";
+import "../Card/style.css";
 
 class Card extends Component {
     constructor(props) {
--- client/src/components/Testimonial/index.js
+++ client/src/components/Testimonial/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 //import { Link } from "react-router-dom";
 import "./style.css";
+import Wrapper from "../Wrapper";
 
 
 function testimonial() {
     return (
         <div>
-        <div class="row">
-            <div class="col m4">
-                <div class="card blue-grey darken-1">
-                    <div class="card-content white-text">
-                    <div class="text-center"><i class="fas fa-utensils fa-5x"></i></div>
+        <Wrapper>
+        <div className="row">
+            <div className="col m4">
+                <div className="card blue-grey darken-1">
+                    <div className="card-content white-text">
+                    <div className="text-center"><i class="fas fa-utensils fa-5x"></i></div>
                         <div>
-                        <span class="card-title text-center"><h3>Share Food</h3></span>
+                        <span className="card-title text-center"><h3>Share Food</h3></span>
                         <div>
-                        <p class="quote">"I have an organic garden and use Neighborhood Pantry to let my neighbors know what extras I have before my vegetables spoil."</p>
-                        <p class="name">- Jean</p>
+                        <p className="quote">"I have an organic garden and use Neighborhood Pantry to let my neighbors know what extras I have before my vegetables spoil."</p>
+                        <p className="name">- Jean</p>
                         </div>
                         </div>
                     </div>
@@ -24,14 +26,14 @@ function testimonial() {
         
 
         {/* <div class="row"> */}
-        <div class="col m4">
-            <div class="card blue-grey darken-1">
-                <div class="card-content white-text">
-                <div class="text-center"><i class="far fa-money-bill-alt fa-5x"></i></div>
+        <div className="col m4">
+            <div className="card blue-grey darken-1">
+                <div className="card-content white-text">
+                <div className="text-center"><i class="far fa-money-bill-alt fa-5x"></i></div>
                 <div>
-                    <span class="card-title text-center"><h3>Save Money</h3></span>
-                    <p class="quote">"I'm always eating and love taking my friends' freebies!"</p>
-                    <p class="name">- Matt</p>
+                    <span className="card-title text-center"><h3>Save Money</h3></span>
+                    <p className="quote">"I'm always eating and love taking my friends' freebies!"</p>
+                    <p className="name">- Matt</p>
                     </div>
                 </div>
             </div>
@@ -39,14 +41,14 @@ function testimonial() {
         {/* </div> */}
 
         {/* <div class="row"> */}
-        <div class="col m4">
-            <div class="card blue-grey darken-1">
-                <div class="card-content white-text">
-                <div class="text-center"><i class="fas fa-seedling fa-5x"></i></div>
+        <div className="col m4">
+            <div className="card blue-grey darken-1">
+                <div className="card-content white-text">
+                <div className="text-center"><i class="fas fa-seedling fa-5x"></i></div>
                 <div>
-                    <span class="card-title text-center"><h3>Reduce Waste</h3></span>
-                    <p class="quote">"I'm passionate about environmental issues and this is a great way to make a difference."</p>
-                    <p class="name">- Jennifer</p>
+                    <span className="card-title text-center"><h3>Reduce Waste</h3></span>
+                    <p className="quote">"I'm passionate about environmental issues and this is a great way to make a difference."</p>
+                    <p className="name">- Jennifer</p>
                     </div>
                 </div>
             </div>
@@ -54,7 +56,7 @@ function testimonial() {
         </div>
 
         {/* </div> */}
-
+        </Wrapper>
         </div>
 
     );
